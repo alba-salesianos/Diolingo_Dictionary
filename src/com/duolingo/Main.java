93b36b146853d432ec429df6e54c7ff8af2aee5c
alba-salesianos <usr3596@salesianos-lacuesta.net>
2023-05-12 10:01:09 +0100

    fix: oops i created a commit without adding files; added Word class

--- src/com/duolingo/Main.java
+++ src/com/duolingo/Main.java
@@ -1,7 +1,19 @@
 package com.duolingo;
 
+import com.duolingo.entities.Word;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Word word = new Word("Banco","Sustantivo");
+
+        word.addDefinition("Sitio para sentarse");
+        word.addDefinition("Empresa que causó la crisis de 2007");
+
+
+
+        System.out.println(word);
+
+
     }
 }
--- src/com/duolingo/entities/Word.java
+++ src/com/duolingo/entities/Word.java
@@ -1,2 +1,46 @@
-package com.duolingo.entities;public class Word {
+package com.duolingo.entities;
+
+import java.util.ArrayList;
+
+public class Word {
+    private String lemma;
+    private String category;
+    private ArrayList<String> definition = new ArrayList<>();
+
+    public Word(String lemma, String category) {
+        this.lemma = lemma;
+        this.category = category;
+    }
+
+    public String getLemma() {
+        return this.lemma;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public ArrayList<String> getDefinition() {
+        return this.definition;
+    }
+
+    public void addDefinition(String definitions){
+        definition.add(definitions);
+    }
+
+    public String displayDefinitions(){
+        String msg = "  - ";
+
+        msg += definition.toString().replace("[","").replace("]","").replace(",","\n  -");
+        return msg;
+    }
+
+    @Override
+    public String toString() {
+        String msg = lemma + "\n" +
+                "---------------------------------\n" +
+                "* Categoría: " + category + "\n" +
+                "* Definición:\n" + displayDefinitions();
+        return msg;
+    }
 }
